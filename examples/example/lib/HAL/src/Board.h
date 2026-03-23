c32e776ca7180f42454c9341476d22259f54602b
Felix Reitenauer <Felix.Reitenauer@newtec.de>
2026-03-23 14:23:43 +0100

    Added initial example structure

---
+++ examples/example/lib/HAL/src/Board.h
@@ -0,0 +1,104 @@
+/* MIT License
+ *
+ * Copyright (c) 2023 - 2026 Andreas Merkle <web@blue-andi.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*******************************************************************************
+    DESCRIPTION
+*******************************************************************************/
+/**
+ * @brief  The physical robot board realization.
+ * @author 
+ * 
+ * @addtogroup HALTarget
+ *
+ * @{
+ */
+#ifndef BOARD_H
+#define BOARD_H
+
+/******************************************************************************
+ * Compile Switches
+ *****************************************************************************/
+
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+#include <stdint.h>
+#include <IBoard.h>
+
+/******************************************************************************
+ * Macros
+ *****************************************************************************/
+
+/******************************************************************************
+ * Types and Classes
+ *****************************************************************************/
+
+/**
+ * The concrete physical board.
+ */
+class Board : public IBoard
+{
+public:
+
+    /**
+     * Get board instance.
+     * 
+     * @return Board instance
+     */
+    static Board& getInstance()
+    {
+        static Board instance; /* idiom */
+
+        return instance;
+    }
+
+    /**
+     * Initialize the hardware.
+     */
+    void init() final;
+
+
+protected:
+
+private:
+
+    /**
+     * Constructs the concrete board.
+     */
+    Board();
+
+    /**
+     * Destroys the concrete board.
+     */
+    ~Board()
+    {
+    }
+
+};
+
+/******************************************************************************
+ * Functions
+ *****************************************************************************/
+
+#endif /* BOARD_H */
+/** @} */
