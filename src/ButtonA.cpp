b998d05e0851bd65386349c4be68de63880fca3b
Felix Reitenauer <Felix.Reitenauer@newtec.de>
2026-03-24 12:05:31 +0100

    chore: update copyright date and add author name to HAL template

--- src/ButtonA.cpp
+++ src/ButtonA.cpp
@@ -1,6 +1,6 @@
 /* MIT License
  *
- * Copyright (c) 2023 - 2026 Andreas Merkle <web@blue-andi.de>
+ * Copyright (c) 2026 - 2026 Andreas Merkle <web@blue-andi.de>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@
 *******************************************************************************/
 /**
  * @brief  
- * @author 
+ * @author Felix Reitenauer
  */
 
 /******************************************************************************
